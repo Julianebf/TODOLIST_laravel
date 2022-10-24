40b6297121a8a3a8daf8893481f0ea9a94269ed3
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-10-24 01:38:12 -0300

    adicionando views

---
+++ todoLIST/resources/views/new_task_frm.blade.php
@@ -0,0 +1,16 @@
+@extends('main_layout')
+
+@section('content')
+    <div class="row">
+        <div class="col s6 offset-6 card #00bfa5 teal accent-4">
+            <div class="card">  
+                <div class="card-action">
+                    <h3> Todo List </h3>
+
+                    <hr>
+                    <h3> New Task </h3>
+                </div>
+            </div>
+        </div>
+    </div>         
+@endsection
