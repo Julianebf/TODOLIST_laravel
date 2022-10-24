5891660c82e27424ef51a0bee194dc3a3f0ce2b5
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-10-24 16:45:02 -0300

    editando views, acrescentando a tabela do sql

--- todoLIST/app/Http/Controllers/Main.php
+++ todoLIST/app/Http/Controllers/Main.php
@@ -12,8 +12,9 @@ class Main extends Controller
 {
     public function home (){
 
-        $tasks = Task::all();
-        //$tasks = Task::where('visible', 1)->get();
+        $tasks = Task::where('visible', 1)
+                ->orderBy('created_at', 'desc')
+                ->get();
 
         return view('home', ['tasks' => $tasks]);           
     }
@@ -22,9 +23,87 @@ class Main extends Controller
         return view("new_task_frm");
     }
 
-    public function new_task_submit(Request $r){
-        $texto = $r->input('text_new_task');
-        echo $texto;
+    public function new_task_submit(Request $request){
+        
+
+        $new_task = $request->input('text_new_task');
+        
+        $task = new Task();
+        $task->task = $new_task;
+        $task->save();
+
+        return redirect()->route('home');
+
+
+    }
+
+    public function task_done($id){
+        
+        $task = Task::find($id);
+        $task->done = new \DateTime();
+        $task->save();
+
+        return redirect()->route('home');
+
+    }
+
+    public function task_undone($id){
+        
+        $task = Task::find($id);
+        $task->done = null;
+        $task->save();
+
+        return redirect()->route('home');
+
+    }
+
+    public function edit_task($id){
+       
+        $task = Task::find($id);
+        
+        return view('edit_task_frm',['task' => $task]);
+        
+        
+    }
+
+    public function edit_task_submit(Request $request){
+        
+        $id_task = $request->input('id_task');
+        $edit_task = $request->input('text_edit_task');
+
+        $task = Task::find($id_task);
+        $task->task = $edit_task;
+        $task->save();
+
+        return redirect()->route('home');
+
+    }
+
+    public function task_invisible($id){
+        
+        $task = Task::find($id);
+        $task->visible =0;
+        $task->save();
+
+        return redirect()->route('home');
+    }
+
+    public function task_visible($id){
+        $task = Task::find($id);
+        $task->visible =1;
+        $task->save();
+
+        return redirect()->route('home');
+        
+    }
+
+    public function list_invisible(){
+        
+        $tasks = Task::where('visible', 0)
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+        return view('home', ['tasks' => $tasks]);   
         
     }
 }
--- todoLIST/resources/views/edit_task_frm.blade.php
+++ todoLIST/resources/views/edit_task_frm.blade.php
@@ -5,17 +5,19 @@
         <div class="col s6 offset-6 card #00bfa5 teal accent-4">
             <div class="card">  
                 <div class="card-action">
-                    <h4 class="teal-text"> Nova Tarefa </h4>
-                    <form action="{{route('new_task_submit')}}" method="post">
+                    <h4 class="teal-text"> Editar Tarefa </h4>
+                    <form action="{{route('edit_task_submit')}}" method="post">
                         <div class="row">
                             <form class="col s12">
                               <div class="row">
                                 @csrf
+                                <input type="hidden" name="id_task" value="{{$task->id}}">
+                                <input type="checkbox" />
                                 <div class="input-field col s12">
-                                  <textarea class="materialize-textarea" name="text_new_task" id=text_new_task></textarea>
-                                  <label for="text_new_task">Escreva a nova tarefa:</label>
-                                  <button class="btn waves-effect waves-light" type="submit">Salvar
-                                    <i class="material-icons right">playlist_add_check</i>
+                                  <textarea class="materialize-textarea" name="text_edit_task" id=text_edit_task value="{{$task->$task}}"></textarea>
+                                  <label for="text_edit_task">Edite a tarefa:</label>
+                                  <button class="btn waves-effect waves-light" type="submit">Editar
+                                    <i class="material-icons right">border_color</i>
                                   </button>
                                   <a href="{{route('home')}}" class="waves-effect waves-light btn red">Cancelar <i class="material-icons right">close</i></a>
                                 </div>
